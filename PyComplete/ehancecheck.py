adc547daaaad2b09e2d03fb13fbbf3eba6f6c9d4
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-03-23 00:16:14 +0530

    Add files via upload

---
+++ PyComplete/ehancecheck.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 19:06:38 2021
+
+@author: HP
+"""
+
+value = int(input("Please enter an integer value in the range 0...10: "))
+if value >= 0: 
+    if value <= 10:
+        print(value, "is in range")
+    else:
+        print(value, "is too large")
+else:
+    print(value, "is too small")
+print("Done")       
+    # First check
+ # Second check
+
+
+
+
+
