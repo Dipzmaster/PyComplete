79e7323f346fb9b27e4bda4d7e79d84bd66ac86c
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-03-28 00:18:12 +0530

    Add files via upload

---
+++ PyComplete/Enter.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 16:03:20 2021
+
+@author: HP
+"""
+#Shortest code structure
+
+print('Please enter an integer value:')
+x = input()
+print('Please enter another integer value:')
+y = input()
+num1 = int(x)
+num2 = int(y)
+print(num1, '+', num2, '=', num1 + num2)
+
+x = input('Please enter an integer value: ')
+y = input('Please enter another integer value: ')
+num1 = int(x)
+num2 = int(y)
+print(num1, '+', num2, '=', num1 + num2)
+
+num1 = int(input('Please enter an integer value: '))
+num2 = int(input('Please enter another integer value: '))
+print(num1, '+', num2, '=', num1 + num2)
