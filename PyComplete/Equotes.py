176743d12a2e2b936ccf35cc33a228d8c22d15d5
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-03-29 00:16:20 +0530

    Add files via upload

---
+++ PyComplete/Equotes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 16:01:13 2021
+
+@author: HP
+"""
+
+print("Did you know that 'word' is a word?")
+print('Did you know that "word" is a word?')
+print('Did you know that \'word\' is a word?')
+print("Did you know that \"word\" is a word?")
+
+filename = 'C:\\Users\\sick'
+print(filename)
+
+print('Please enter some text:')
+x = input()
+print('Text entered:', x)
+print('Type:', type(x))
---
+++ PyComplete/expression.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 16:22:28 2021
+
+@author: HP
+"""
+
+value1 = int(input('Please enter a number: '))
+value2 = int(input('Please enter another number: '))
+sum = value1 + value2
+print(value1, '+', value2, '=', sum)
+
+print(25%4, 4%25)
