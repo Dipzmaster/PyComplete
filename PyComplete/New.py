553e7863190f2c92e930f0b9f8550bb3c21df9ac
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-04-03 00:17:44 +0530

    Add files via upload

---
+++ PyComplete/New.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 15:37:33 2021
+
+@author: HP
+"""
+
+print("This is a simple Python program")
---
+++ PyComplete/multiway.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 19:13:54 2021
+
+@author: HP
+"""
+
+# Use a mult-way conditional statement
+value = int(input("No : "))
+if value == 0:
+    print("zero")
+elif value == 1:
+    print("one")
+elif value == 2:
+    print("two")
+elif value == 3:
+    print("three")
+elif value == 4:
+    print("four")
+elif value == 5:
+    print("five")
+print("Done")
