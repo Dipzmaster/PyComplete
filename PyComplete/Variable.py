0936a0dab69ee53af9dad1bde4132a11a48a0187
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-04-09 14:15:58 +0530

    Add files via upload

---
+++ PyComplete/Variable.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 15:54:57 2021
+
+@author: HP
+"""
+
+a = 10
+print('First, variable a has value', a, 'and type', type(a))
+a = 'ABC'
+print('Now, variable a has value', a, 'and type', type(a))
+
+del a
+
+pi = 3.14159
+print("Pi =", pi)
+print("or", 3.14, "for short")
+
+avogadros_number = 6.022e23
+c = 2.998e8
+print("Avogadro's number =", avogadros_number)
+print("Speed of light =", c)
+
+
+print('A\nB\nC')
+print('D\tE\tF')
+print('WX\bYZ')
+print('1\a2\a3\a4\a5\a6')
